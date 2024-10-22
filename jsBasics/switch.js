0ee582f157088dfb92912249cb622cf75b8d39d1
TheProgrammerofalltime <kma69362@gmail.com>
2024-10-21 18:23:09 -0700

    commit

---
+++ jsBasics/switch.js
@@ -0,0 +1,41 @@
+let weather = 'rainy';
+switch(weather){
+    case'sunny':
+        console.log("Wear Sunglasses.")
+        break;
+    case'rainy':
+        console.log("Take an Umbrella today.")
+        break;
+    case'cloudy':
+        console.log("You might need a light jacket.")
+        break;
+    case'snowy':
+        console.log("You should wear a coat and gloves today.")
+        break;
+    default:
+        console.log("The weather is unpredictable, I guess.")
+}
+
+let day = '6';
+switch(day){
+    case '1':
+        console.log('Today is Monday.')
+        break;
+    case '2':
+        console.log('Today is Tuesday.')
+        break;
+    case '3':
+        console.log('Today is Wednesday.')
+        break;
+    case '4':
+        console.log('Today is Thrusday.')
+        break;
+    case '5':
+        console.log('Today is Friday.')
+        break;
+    case '6':
+        console.log('Today is Saturday.')
+        break;
+    default:
+        console.log('Today is Sunday.')
+}
