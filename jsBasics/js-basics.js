4c0327259a24db03ea91d7d23182ba142c511bd8
TheProgrammerofalltime <kma69362@gmail.com>
2024-10-30 18:19:58 -0700

    commit

--- jsBasics/js-basics.js
+++ jsBasics/js-basics.js
@@ -173,97 +173,178 @@ switch(login){
     default:
         message1 = '';
 }
-console.log(message1)
+// console.log(message1)
+// 
+// let pen = true
+// let paper = true
+// let mouse = false
+// let keyboard = true
+// 
+// if(pen && paper == true || mouse && keyboard == true){
+//     console.log('You can take notes')
+// } else {
+//     console.log('You cannot take notes')
+// }
+// 
+// let allNighter = false
+// let didNotStudy = false
+// let tooManyGames = false
+// let skippedBreakfast = false
+// 
+// if(!allNighter && !tooManyGames && !didNotStudy && !skippedBreakfast){
+//     console.log('Bro is locked in fr fr')
+// } else {
+//     console.log('Bro is so cooked lmao')
+// }
+// 
+// let location = null
+// let geoLocation;
+// 
+// console.log(geoLocation ?? 'Not a valid geo location')
+// let newLocation = location ?? 'Arizona'
+// console.log(newLocation)
+// 
+// let number1 = Math.floor(Math.random() * 100)
+// let number2 = Math.floor(Math.random() * 100)
+// let number3 = Math.floor(Math.random() * 100)
+// let number4 = Math.floor(Math.random() * 100)
+// let number5 = Math.floor(Math.random() * 100)
+// 
+// console.log(number1 + ',', number2 + ',', number3 + ',', number4 + ',', number5)
+// 
+// for(var i = 10; i > 1; i--){
+//     console.log(`This has happened ${i} times`)
+// }
+// 
+// for(var i = 5; i < 14; i+= 2){
+//     console.log(i)
+// }
+// 
+// for(var i = 5; i < 12; i++){
+//     console.log(i)
+// }
+// 
+// console.log('---------------------')
+// 
+// for(var i = 1; i < 10; i++){
+//     console.log("this is nice")
+// }
+// 
+// console.log('---------------------')
+// 
+// for(var i = 11; i > 0; i-=2){
+//     console.log(i)
+// }
+// 
+// let i = 0;
+// while(i < 3){
+//     console.log(i);
+//     i++;
+// }
+// 
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// } while (i < 3)
+// 
+// var classes = 12
+// 
+// for(var i = 0; i < classes; i++){
+//     if(i==7){
+//         console.log("The next frontier")
+//         continue
+//     }
+//     console.log(i)
+//     console.log("This should always print")
+// }
+// 
+// outer: for(var i = 0; i < 3; i++){
+//     for(var j = 0; j < 5; j++){
+//         let input = prompt("What is your name?")
+//         if(input.length > 0){
+//             break outer;
+//         }
+//     }
+// }
+// 
+// let ages = [5,12,18,21,58];
+// 
+// for(var i = 0; i < ages.length; i++){
+//     console.log(ages[i])
+// }
+// 
+// ages[6] = 74
+// console.log(ages)
+// 
+// let a = ["red", "green", "orange", "yellow", "blue"];
+// a[4] = "purple"
+// console.log(a)
+// 
+// a.push("Salmon", "Seafoam Green")
+// console.log(a)
+// 
+// a[2] = "Black"
+// a.pop()
+// console.log(a)
+// 
+// a.shift()
+// console.log(a)
+// 
+// a.unshift("Crimson", "Forest", "Lavender")
+// console.log(a)
+// 
+// for(let colors of a){
+//     colors +=" - Solid"
+//     console.log(colors)
+// }
+// 
+// let music = ["jazz", "blues"];
+// 
+// music.push("R&B");
+// 
+// if(music.length % 2 !== 0){
+//     const middleIndex = Math.floor(music.length / 2);
+//     music[middleIndex] = "none";
+// }
+// 
+// const middleIndex = Math.floor(music.length / 2);
+// music = [...music.slice(0, middleIndex), "classic", ...music.slice(middleIndex)];
+// 
+// music = ["rap", "acapella", ...music];
+// 
+// console.log(music)
+// 
+// let b = ["Mordecai", "Rigby", "Benson", "Skips", "Muscle Man"]
+// let c = ["Jake", "Finn", "LSP", "Marceline", "Princess Bubblegum", "Ice King"]
+// 
+// let e = ["BMO", ...c, "The Lich", "Flame Princess"]
+// console.log(e)
+// 
+// let f = [...b, ...c]
+// console.log(f)
+// 
+// let words = "Who would stop talking first? Rohan, Monte, or Justin? I'm not adding that sentence."
+// console.log(words.split("i"));
+// 
+// let newWords=words.split(" ")
+// console.log(newWords)
+// 
+// console.log( typeof newWords)
+// 
+// let g = newWords.join("i/er")
+// console.log(g)
+// 
+// console.log("Hawk 1: What's 2 + 2?")
+// console.log("Hawk 2: Uhh...")
 
-let pen = true
-let paper = true
-let mouse = false
-let keyboard = true
+let words = "Who would stop talking first? Rohan, Monte, or Justin?"
+let words2 = "The real problem is always those wierd meat like people with feet and more opinions than needed."
+let newWords=words.split(" ")
+let newWords2=words2.split(" ")
 
-if(pen && paper == true || mouse && keyboard == true){
-    console.log('You can take notes')
-} else {
-    console.log('You cannot take notes')
-}
-
-let allNighter = false
-let didNotStudy = false
-let tooManyGames = false
-let skippedBreakfast = false
-
-if(!allNighter && !tooManyGames && !didNotStudy && !skippedBreakfast){
-    console.log('Bro is locked in fr fr')
-} else {
-    console.log('Bro is so cooked lmao')
-}
-
-let location = null
-let geoLocation;
-
-console.log(geoLocation ?? 'Not a valid geo location')
-let newLocation = location ?? 'Arizona'
-console.log(newLocation)
-
-let number1 = Math.floor(Math.random() * 100)
-let number2 = Math.floor(Math.random() * 100)
-let number3 = Math.floor(Math.random() * 100)
-let number4 = Math.floor(Math.random() * 100)
-let number5 = Math.floor(Math.random() * 100)
-
-console.log(number1 + ',', number2 + ',', number3 + ',', number4 + ',', number5)
-
-for(var i = 10; i > 1; i--){
-    console.log(`This has happened ${i} times`)
-}
-
-for(var i = 5; i < 14; i+= 2){
-    console.log(i)
-}
-
-for(var i = 5; i < 12; i++){
-    console.log(i)
-}
-
-console.log('---------------------')
-
-for(var i = 1; i < 10; i++){
-    console.log("this is nice")
-}
-
-console.log('---------------------')
-
-for(var i = 11; i > 0; i-=2){
-    console.log(i)
-}
-
-let i = 0;
-while(i < 3){
-    console.log(i);
-    i++;
-}
-
-let i = 0;
-do{
-    console.log(i);
-    i++;
-} while (i < 3)
-
-var classes = 12
-
-for(var i = 0; i < classes; i++){
-    if(i==7){
-        console.log("The next frontier")
-        continue
-    }
-    console.log(i)
-    console.log("This should always print")
-}
-
-outer: for(var i = 0; i < 3; i++){
-    for(var j = 0; j < 5; j++){
-        let input = prompt("What is your name?")
-        if(input.length > 0){
-            break outer;
-        }
-    }
-}
+let g = newWords.join("-")
+let f = newWords2.join("-")
+ 
+console.log(g)
+console.log(f)
