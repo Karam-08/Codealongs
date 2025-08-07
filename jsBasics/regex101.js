f7277fc024487f0e532a01070a6883c72c578d94
Karam Abbas <kma69362@gmail.com>
2025-08-07 14:35:27 -0700

    regex101

---
+++ jsBasics/regex101.js
@@ -0,0 +1,25 @@
+// let str = "Hello, world!";
+// let pattern = /world/;
+// str.search(pattern);
+// str.search(/cat/);
+// 
+// let text = "The cat in the hat."
+// let pattern2 = /.at/;
+// text.match(pattern);
+// 
+// let phone = "My number is 555-123-4567"
+// let pattern3 = /\d\d\d/;
+// phone.match(pattern)
+
+// let username = "user_123";
+// let pattern = /[e]\w\w\w\w\w/;
+// console.log(username.match(pattern));
+
+// let sentence = "Hello   world.";
+// let pattern = /o\sw/;
+// console.log(sentence.match(pattern));
+
+// let fruit = "apple";
+// let pattern = /[aeiou]/
+// console.log(fruit.match(pattern));
+
